ab8666e1a75375fe7b09a138253bd548c5d92727
Yeuoly <admin@srmxy.cn>
2019-12-29 00:01:57 +0800

    delete:iconfont modified:float-helper

--- src/components/items/ThanksList.vue
+++ src/components/items/ThanksList.vue
@@ -5,7 +5,6 @@
         <li><a href="https://www.biliob.com">见齐</a>开源的前端设计源码</li>
         <li><a href="https://www.vuejs.org">尤大</a>开源的Vue框架</li>
         <li><a href="https://next.vuetifyjs.com">vuetify</a>的UI库</li>
-        <li><a href="https://www.iconfont.cn">阿里</a>的免费矢量图库</li>
     </MaterialCard>
 </template>
 
